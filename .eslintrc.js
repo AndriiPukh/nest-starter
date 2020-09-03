b7f7c1fc33aa7a1e2ef9f29b70f901484c671b20
Andrii Pukh <puschock.andrey@gmail.com>
2020-09-03 15:08:19 +0300

    Add eslint, husky

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	env: {
+		browser: true,
+	},
+	extends: [
+		'google',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never',
+			},
+		],
+	},
+};
